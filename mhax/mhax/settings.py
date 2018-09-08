4b9ab292e636e6356bcfcb978dfa090d829a1cae
Trishul Nagenalli <tn74@duke.edu>
2018-09-08 15:03:43 -0400

    files moved

--- mhax/mhax/settings.py
+++ mhax/mhax/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+### 1UpHealth Info
+
+CLIENT_ID = 'b28ff1070b5f48b08380d5f6da21fe79'
+CLIENT_SECRET = '4WLQ7K3H55A8FaLe1mOtS5W3n7vVLnRx'
+
+TOKEN_URL = 'https://api.1up.health/fhir/oauth2/token
+API_URL = 'https://api.1up.health/fhir'
+SCOPE = 'user/*.*'
